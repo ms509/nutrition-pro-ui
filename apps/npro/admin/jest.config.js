663268ada00e94a4ac6b76ed46c0c223e63126dd
Manju Raghavendra Bellamkonda <raghavb509@gmail.com>
2019-02-11 16:50:46 -0500

    Created NX Project

---
+++ apps/npro/admin/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'npro-admin',
+  preset: '../../../jest.config.js',
+  coverageDirectory: '../../../coverage/apps/npro/admin/'
+};
---
+++ apps/npro/user/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'npro-user',
+  preset: '../../../jest.config.js',
+  coverageDirectory: '../../../coverage/apps/npro/user/'
+};
