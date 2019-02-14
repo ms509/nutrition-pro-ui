dcb5909a8ea96649a43dd47dd12d43f967625d51
Manju Raghavendra Bellamkonda <raghavb509@gmail.com>
2019-02-14 14:42:36 -0500

    Added Login module to npro-admin

--- apps/npro/admin/src/app/app.module.ts
+++ apps/npro/admin/src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule }    from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { routing }        from './app.routing';
 import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { JwtModule } from '@auth0/angular-jwt';
+
+import { AuthGuard } from './_guards';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { AlertService, AuthenticationService, UserService } from './_services';
+import { HomeComponent, LoginComponent, RegisterComponent } from './components';
+import { AlertComponent } from './_directives';
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoginComponent, RegisterComponent, HomeComponent, AlertComponent],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     NxModule.forRoot(),
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    HttpClientModule,
+    routing
   ],
-  providers: [],
+  providers: [
+        AuthGuard,
+        AlertService,
+        AuthenticationService,
+        UserService,
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+        // provider used to create fake backend
+        fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
